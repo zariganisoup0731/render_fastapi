f0ac7148727d28a24b0e86e817cceb02b442b811
zariganisoup0731 <zariganisoup0731@gmail.com>
2026-06-12 12:19:44 +0900

    update present

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+    
+@app.post("/present")
+async def give_present(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
 
 from fastapi.responses import HTMLResponse
 
@@ -67,7 +71,3 @@ def index():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
-
-@app.post("/present")
-async def give_present(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
